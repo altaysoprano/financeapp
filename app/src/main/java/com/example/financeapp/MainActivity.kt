4289f1c01e0dc1f5a6151859e186d7ede1f8e40c
Hasan Cihad Altay <caltayzzz@gmail.com>
2023-07-12 19:49:59 +0300

    adding transaction is done

--- app/src/main/java/com/example/financeapp/MainActivity.kt
+++ app/src/main/java/com/example/financeapp/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.financeapp.data.model.Transaction
 import com.example.financeapp.databinding.ActivityMainBinding
+import com.example.financeapp.util.UiState
 import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
@@ -16,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var transactionAdapter: TransactionAdapter
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var binding : ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        transactions = arrayListOf()
 
         if(firebaseAuth.currentUser == null) {
             val intent = Intent(this, SignInActivity::class.java)
@@ -30,6 +39,32 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
+        viewModel.getTransactions()
+        viewModel.transaction.observe(this) { state ->
+            when(state) {
+                is UiState.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+                is UiState.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(this, state.error.toString(), Toast.LENGTH_SHORT).show()
+                }
+                is UiState.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    transactionAdapter.setData(state.data.toMutableList())
+                }
+            }
+        }
+
+        transactionAdapter = TransactionAdapter(transactions)
+        linearLayoutManager = LinearLayoutManager(this)
+
+        binding.recyclerview.apply {
+            adapter = transactionAdapter
+            layoutManager = linearLayoutManager
+        }
+
+/*
         transactions = arrayListOf(
             Transaction("Weekend Budget", 400.00),
             Transaction("Bananas", -4.00),
@@ -40,13 +75,7 @@ class MainActivity : AppCompatActivity() {
             Transaction("Car Park", -15.00)
             )
 
-        transactionAdapter = TransactionAdapter(transactions)
-        linearLayoutManager = LinearLayoutManager(this)
-
-        binding.recyclerview.apply {
-            adapter = transactionAdapter
-            layoutManager = linearLayoutManager
-        }
+ */
 
         binding.addBtn.setOnClickListener {
             val intent = Intent(this, AddTransactionActivity::class.java)
