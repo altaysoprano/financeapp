1c06c06918c4ea71e6fbd429e27e3a224004eee7
Hasan Cihad Altay <caltayzzz@gmail.com>
2023-07-12 22:12:29 +0300

    user adding is done

--- app/src/main/java/com/example/financeapp/SignUpActivity.kt
+++ app/src/main/java/com/example/financeapp/SignUpActivity.kt
@@ -3,14 +3,20 @@ package com.example.financeapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.example.financeapp.databinding.ActivitySignUpBinding
+import com.example.financeapp.util.UiState
 import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private val viewModel: SignUpViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,10 +25,6 @@ class SignUpActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.textView.setOnClickListener {
-
-        }
-
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
@@ -30,6 +32,8 @@ class SignUpActivity : AppCompatActivity() {
 
             if(email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
                 if(pass == confirmPass) {
+                    viewModel.signUp(email, pass)
+/*
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
                         if(it.isSuccessful) {
                             val intent = Intent(this, SignInActivity::class.java)
@@ -39,6 +43,7 @@ class SignUpActivity : AppCompatActivity() {
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                         }
                     }
+*/
                 } else {
                     Toast.makeText(this, "Şifreler uyuşmuyor", Toast.LENGTH_SHORT).show()
                 }
@@ -46,6 +51,31 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this, "Email, şifre veya onay şifresi boş bırakılamaz !", Toast.LENGTH_SHORT).show()
             }
         }
+
+        viewModel.signUpState.observe(this) { state ->
+            when(state) {
+                is UiState.Loading -> {
+                    binding.progressBarSignIn.visibility = View.VISIBLE
+                }
+                is UiState.Failure -> {
+                    binding.progressBarSignIn.visibility = View.GONE
+                    Toast.makeText(this, state.error.toString(), Toast.LENGTH_SHORT).show()
+                }
+
+                is UiState.Success -> {
+                    binding.progressBarSignIn.visibility = View.GONE
+                    Toast.makeText(this, "Successfully signed up", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, SignInActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+                else -> {
+
+                }
+            }
+        }
+
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
